dbd84c023af41c67719c38e4f6319ce6b22b73d9
Nick Tan <nctan1002@gmail.com>
2020-01-30 00:06:35 -0500

    Added shape and colour changing functionality

--- js/index.js
+++ js/index.js
@@ -1,16 +1,63 @@
 $(document).ready(function(){
 
     // init variables
-    let selectCanvas;
+    let selectCanvas; // canvas object
+    let selectCanvasId; // canvas id
+    let selectShape; // shape id
+    let selectColour; // colour id
+    let activeCanvas; // canvas context
 
-    $("p").click(function(){
-      $(this).hide();
+    let canvases = {
+        "nw-canvas": {
+            shape: "",
+            colour: "#000000"            
+        },
+        "ne-canvas": {
+            shape: "",
+            colour: "#000000"
+        },
+        "se-canvas": {
+            shape: "",
+            colour: "#000000"
+        },
+        "sw-canvas": {
+            shape: "",
+            colour: "#000000"
+        },
+    };
+
+    $("#clear-btn").click(function() {
+        console.log(selectCanvas)
+        clearCanvas(activeCanvas,selectCanvas);
+        canvases[selectCanvasId] = {};
     });
 
     // activate target canvas
     $(".canvas").click(function() {
-        selectCanvas = $(this).attr("id");
-        console.log(selectCanvas);
+        selectCanvas = $(this)[0];
+        selectCanvasId = $(this).attr("id");
+        activeCanvas = document.getElementById(selectCanvasId).getContext("2d");
+        console.log(activeCanvas);
     });
 
-  });
+    // draw selected shape
+    $("#shapes-menu .menu-item").click(function() {
+        selectShape = $(this).attr("id");
+        canvases[selectCanvasId].shape = selectShape;
+        console.log(selectShape);
+        console.log(canvases[selectCanvasId].colour);
+        clearCanvas(activeCanvas,selectCanvas);
+        draw[canvases[selectCanvasId].shape](activeCanvas,canvases[selectCanvasId].colour);
+    });
+
+    // draw selected colour
+    $("#colours-menu .menu-item").click(function() {
+        selectColour = $(this).attr("colour");
+        canvases[selectCanvasId].colour = selectColour;
+        console.log(selectColour);
+        console.log(canvases[selectCanvasId].colour);
+        console.log(canvases[selectCanvasId].colour);
+        clearCanvas(activeCanvas,selectCanvas);
+        draw[canvases[selectCanvasId].shape](activeCanvas,canvases[selectCanvasId].colour);
+    });
+});
--- js/shapes.js
+++ js/shapes.js
@@ -1,25 +1,131 @@
-function drawTriangle(ctx, colour) {
-    function drawcanvas(ctx)
-         {
+function clearCanvas(ctx,canvas) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-               //Shape0;
-               ctx.shadowColor ="rgba(0,0,0,0)";
-               ctx.strokeStyle ="rgba(0,0,0,1)";
-               ctx.lineWidth = 1;
-               ctx.lineCap = "butt";
-               ctx.lineJoin = "miter";
-               ctx.beginPath();
-               ctx.moveTo(0,92);
-               ctx.lineTo(200,199);
-               ctx.lineTo(0,199);
-               ctx.lineTo(0,92);
-               ctx.closePath();
-               ctx.stroke();
-               ctx.shadowOffsetX = 15;
-               ctx.shadowOffsetY = 15;
-               ctx.shadowBlur = 0;
-               ctx.shadowColor = "rgba(0,0,0,0)";
-               ctx.fillStyle = "rgba(242,15,15,1)";
-               ctx.fill();
-         }
+const draw = {
+    "triangle-choice": function drawcanvas(ctx, colour) {
+        console.log('afd')
+            //Shape0;
+            ctx.shadowColor ="rgba(0,0,0,0)";
+            ctx.strokeStyle ="rgba(0,0,0,1)";
+            ctx.lineWidth = 1;
+            ctx.lineCap = "butt";
+            ctx.lineJoin = "miter";
+            ctx.beginPath();
+            ctx.moveTo(0,92);
+            ctx.lineTo(200,199);
+            ctx.lineTo(0,199);
+            ctx.lineTo(0,92);
+            ctx.closePath();
+            ctx.stroke();
+            ctx.shadowOffsetX = 15;
+            ctx.shadowOffsetY = 15;
+            ctx.shadowBlur = 0;
+            ctx.shadowColor = "rgba(0,0,0,0)";
+            ctx.fillStyle = colour;
+            ctx.fill();
+    },
+    "star-choice": function drawcanvas(ctx,colour)
+    {
+          //Shape9;
+          ctx.shadowColor ="rgba(0,0,0,0)";
+          ctx.strokeStyle ="rgba(255,255,255,1)";
+          ctx.lineWidth = 1;
+          ctx.lineCap = "butt";
+          ctx.lineJoin = "miter";
+          ctx.beginPath();
+          ctx.moveTo(20,112);
+          ctx.lineTo(80,92);
+          ctx.lineTo(80,132);
+          ctx.lineTo(20,112);
+          ctx.closePath();
+          ctx.stroke();
+          ctx.shadowOffsetX = -15;
+          ctx.shadowOffsetY = -15;
+          ctx.shadowBlur = 0;
+          ctx.shadowColor = "rgba(0,0,0,0)";
+          ctx.fillStyle = colour;
+          ctx.fill();
+
+          //Shape7;
+          ctx.shadowColor ="rgba(0,0,0,0)";
+          ctx.strokeStyle ="rgba(255,255,255,1)";
+          ctx.lineWidth = 1;
+          ctx.lineCap = "butt";
+          ctx.lineJoin = "miter";
+          ctx.beginPath();
+          ctx.moveTo(100,32);
+          ctx.lineTo(80,92);
+          ctx.lineTo(120,92);
+          ctx.lineTo(100,32);
+          ctx.closePath();
+          ctx.stroke();
+          ctx.shadowOffsetX = 15;
+          ctx.shadowOffsetY = 15;
+          ctx.shadowBlur = 0;
+          ctx.shadowColor = "rgba(0,0,0,0)";
+          ctx.fillStyle = colour;
+          ctx.fill();
+
+          //Shape10;
+          ctx.shadowColor ="rgba(0,0,0,0)";
+          ctx.strokeStyle ="rgba(255,255,255,1)";
+          ctx.lineWidth = 1;
+          ctx.lineCap = "butt";
+          ctx.lineJoin = "miter";
+          ctx.beginPath();
+          ctx.moveTo(80,92);
+          ctx.lineTo(120,92);
+          ctx.lineTo(120,132);
+          ctx.lineTo(80,132);
+          ctx.lineTo(80,92);
+          ctx.closePath();
+          ctx.stroke();
+          ctx.shadowOffsetX = 15;
+          ctx.shadowOffsetY = 15;
+          ctx.shadowBlur = 0;
+          ctx.shadowColor = "rgba(0,0,0,0)";
+          ctx.fillStyle = colour;
+          ctx.fill();
+
+          //Shape11;
+          ctx.shadowColor ="rgba(0,0,0,0)";
+          ctx.strokeStyle ="rgba(255,255,255,1)";
+          ctx.lineWidth = 1;
+          ctx.lineCap = "butt";
+          ctx.lineJoin = "miter";
+          ctx.beginPath();
+          ctx.moveTo(180,112);
+          ctx.lineTo(120,132);
+          ctx.lineTo(120,92);
+          ctx.lineTo(180,112);
+          ctx.closePath();
+          ctx.stroke();
+          ctx.shadowOffsetX = -15;
+          ctx.shadowOffsetY = -15;
+          ctx.shadowBlur = 0;
+          ctx.shadowColor = "rgba(0,0,0,0)";
+          ctx.fillStyle = colour;
+          ctx.fill();
+
+          //Shape12;
+          ctx.shadowColor ="rgba(0,0,0,0)";
+          ctx.strokeStyle ="rgba(255,255,255,1)";
+          ctx.lineWidth = 1;
+          ctx.lineCap = "butt";
+          ctx.lineJoin = "miter";
+          ctx.beginPath();
+          ctx.moveTo(100,192);
+          ctx.lineTo(120,132);
+          ctx.lineTo(80,132);
+          ctx.lineTo(100,192);
+          ctx.closePath();
+          ctx.stroke();
+          ctx.shadowOffsetX = 15;
+          ctx.shadowOffsetY = 15;
+          ctx.shadowBlur = 0;
+          ctx.shadowColor = "rgba(0,0,0,0)";
+          ctx.fillStyle = colour;
+          ctx.fill();
+    }
 }
